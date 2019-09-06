91fd4dff03c105d029937fe38d3e96bc6be8be9c
Danyi Luo <luodanyi0428@gmail.com>
2019-09-06 14:51:11 -0500

    9 Palindrome Number

---
+++ untitled/src/PalindromeNumber.java
@@ -0,0 +1,12 @@
+public class PalindromeNumber {
+    public boolean isPalindrome(int num){
+        if (num < 0) return false;
+        int reversed = 0, remainder, original = num;
+        while (num != 0){
+            remainder = num % 10;
+            reversed = reversed * 10 + remainder;
+            num /= 10; // the last digit is removed from num after division by 10.
+        }
+        return original == reversed;
+    }
+}
